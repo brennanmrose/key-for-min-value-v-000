45d0a1c9848bc7251356323020e216aa3a781f99
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-26 01:44:45 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,11 +7,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   name_hash.each do |name, number|
   lowest_value = number < number  
-      puts lowest_value
-    else 
-      puts nil 
-    end 
-  end
+  lowest_value
 end
 
 #a.sort_by { |key, value| value }.first
