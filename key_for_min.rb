f2501e60f7320969c4eba5dfe783a6ac579ea4d7
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-26 02:11:36 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 require 'pry'
 def key_for_min_value(name_hash)
   min_key = nil
-  min_value = null
+  min_value = nil
   name_hash.each do |name, number|
     binding.pry
       name = number.to_s 
