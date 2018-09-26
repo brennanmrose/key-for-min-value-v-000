2ae1e31c95da4fafcbb32c67564b6680998b453a
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-26 01:51:32 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ require 'pry'
 def key_for_min_value(name_hash)
   name_hash.each do |name, number|
     if number < number  
-      puts name[0]
+      name[0] = (lowest, value)
     else 
       nil 
     end 
