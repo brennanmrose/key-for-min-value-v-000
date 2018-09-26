91274ce0f3f104028bdb139f48416b93fd99f7b3
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-26 02:02:46 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,5 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
 
 #{:blake=>500, :ashley=>2, :adam=>1}
 
@@ -18,18 +16,3 @@ def key_for_min_value(name_hash)
     end 
   end
 end
-
-#   key = k.to_s
-#   eval('key = v')
-#   eval('puts key')
-# end
-
-#a.sort_by { |key, value| value }.first
-
-# birthday_kids.collect do |kids_name, age|
-#     age
-# end
-
-# birthday_kids.collect do |name, age|
-#     age * 7
-# end
