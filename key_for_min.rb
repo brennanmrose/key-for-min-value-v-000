56002fc83e06fa440a19d7c39a565d0672f7370b
Rose Brennan <brennan.m.rose@gmail.com>
2018-09-26 01:46:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,9 +8,6 @@ def key_for_min_value(name_hash)
   name_hash.each do |name, number|
   lowest_value = number < number  
       puts lowest_value[0]
-    else 
-      puts nil 
-    end 
   end
 end
 
